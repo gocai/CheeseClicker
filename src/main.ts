a9d17338352a5dd58dfa1a2f1f564e57c61de2e5
Gordon Cai <gocai@ucsc.edu>
2023-10-11 01:06:59 -0700

    step 2 complete! working button with increment

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-
+//Game Title
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Add more cheese";
@@ -8,4 +8,5 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
 app.append(header);
